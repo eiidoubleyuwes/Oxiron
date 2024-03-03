b5704b44d7cc9df7820d6779dd5b585b6d8ee943
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-03-03 16:58:34 +0300

    Feat:Created the ability to readmessages from the server
    This is just simple backend code

--- client-code/src/main.rs
+++ client-code/src/main.rs
@@ -8,5 +8,26 @@ use std::time::Duration;
 const LOCAL: &str = "127:0:0:1:6000";
 const MSG_SIZE: usize = 1000;
 fn main(){
-    
+    let mut client = TcpStream::connect(LOCAL).expect("Client failed to connect to the server port and IP");
+    client.set_nonblocking(true).expect("Failed to initialize non-blocking");
+
+    let (tx, rx) = mpsc::channel::<String>();   
+
+    thread::spawn(move || loop {
+        
+        let mut buff = vec![0; MSG_SIZE];
+        match client.read_exact(&mut buff){
+            Ok(_) => {
+                let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                println!("Message received: {:?}", msg);
+            },
+            Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+            Err(_) => {
+                println!("Connection with the server was severed");
+                break;
+            }
+        }
+        
+    });
+
 }
