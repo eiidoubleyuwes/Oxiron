ccc02c862d57e5ffb221ba1adf06af0aa5e1cce8
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-03-03 15:29:23 +0300

    Feat:Finishing touches
    As of now The server can receive messages from the clients
    
    The issue is it brodcasts all the messages to all the clients
    
    Maybe later I'll add the ability for closed loop communication

--- server-code/src/main.rs
+++ server-code/src/main.rs
@@ -22,7 +22,40 @@ fn main() {
         //If a client connects, accept the connection and store the client
         if let Ok((mut socket, addr)) = server.accept() {
             println!("Client {} connected", addr);
-            
+            let tx = tx.clone();
+            //Create a new thread to handle the client
+            clients.push(socket.try_clone().expect("Failed to clone client"));
+            thread::spawn(move || loop {
+                let mut buff = vec![0; MSG_SIZE];
+                //Read the message from the client
+                match socket.read_exact(&mut buff) {
+                    Ok(_) => {
+                        let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                        //Collect everything into a single string
+                        let msg = String::from_utf8(msg).expect("Invalid message");
+                        println!("{}: {:?}", addr, msg);
+                        tx.send(msg).expect("Failed to send message");
+                    },
+                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (), //Return a unit type and proceed
+                    //If any other kind of error close the connection
+                    Err(_) => {
+                        println!("Closing connection with: {}", addr);
+                        break;
+                    }
+                }
+                //If a message is received, broadcast it to all clients
+                if let Ok(msg) = rx.try_recv() {
+                    clients = clients.into_iter().filter_map(|mut client| {
+                        //Write the message to the client
+                        let mut buff = msg.clone().into_bytes();
+                        buff.resize(MSG_SIZE, 0);
+                        client.write_all(&buff).map(|_| client).ok()
+                    }).collect::<Vec<_>>();
+                }
+                //Sleep for a short time to avoid using too much CPU
+                thread::sleep(std::time::Duration::from_millis(100));
+            });
+        }
     }
 }
 
