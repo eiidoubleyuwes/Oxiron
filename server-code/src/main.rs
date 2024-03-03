84959ce4767597522ea4ac479891b13e96bbfb5b
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-03-03 15:14:58 +0300

    Feat:Added start of the code
    Here I have defined constants and imported all the necessary things
    
    I have also binded the server to the local port address and the local port numbers

--- server-code/src/main.rs
+++ server-code/src/main.rs
@@ -1,3 +1,17 @@
+//This is the server side code.
+//It will open a socket and listen for clients
+
+use std::net::TcpListener;
+use std::io::{Read, Write, ErrorKind};
+use std::thread;
+use std::sync::mpsc;
+
+//Define constants such ad the IP address and port number
+//The message size is also defined as 1000 bits to facilitate long messages
+const LOCAL: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 1000;
 fn main() {
-    println!("Hello, world!");
+    //Bind the server to the IP address and port number
+    let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
 }
+
