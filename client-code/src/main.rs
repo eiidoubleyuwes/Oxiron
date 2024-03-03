f4fb75eee481a839ab3a3904325a1f8d5e69087b
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-03-03 16:55:31 +0300

    Feat:Sart of the client side code
    This is just a brief definition of constants and all

--- client-code/src/main.rs
+++ client-code/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+//This is the complimentary client code for the server code in the server code file
+use std::io::{self, ErrorKind, Read, Write};
+use std::net::TcpStream;
+use std::sync::mpsc::{self, TryRecvError};
+use std::thread;
+use std::time::Duration;
+
+const LOCAL: &str = "127:0:0:1:6000";
+const MSG_SIZE: usize = 1000;
+fn main(){
+    
 }
