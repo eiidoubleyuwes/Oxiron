17a222c58e9d63ed517ba524b8953ca18311d65f
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-04-08 22:55:28 +0300

    Feat:Added all the old code
    Now this project is done fr

---
+++ Old_code/client-code/src/main.rs
@@ -0,0 +1,54 @@
+//This is the complimentary client code for the server code in the server code file
+use std::io::{self, ErrorKind, Read, Write};
+use std::net::TcpStream;
+use std::sync::mpsc::{self, TryRecvError};
+use std::thread;
+use std::time::Duration;
+
+const LOCAL: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 1000;
+fn lala(){
+   thread::sleep(Duration::from_millis(100));
+}
+fn main(){
+    let mut client = TcpStream::connect(LOCAL).expect("Client failed to connect to the server port and IP,check if server is on");
+    client.set_nonblocking(true).expect("Failed to initialize non-blocking");
+
+    let (tx, rx) = mpsc::channel::<String>();   
+
+    thread::spawn(move || loop {
+
+        let mut buff = vec![0; MSG_SIZE];
+        match client.read_exact(&mut buff){
+            Ok(_) => {
+                let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                println!("Message received: {:?}", msg);
+            },
+            Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+            Err(_) => {
+                println!("Connection with the server was severed");
+                break;
+            }
+        }
+        match rx.try_recv(){
+            Ok(msg) => {
+                let mut buff = msg.clone().into_bytes();
+                buff.resize(MSG_SIZE, 0);
+                client.write_all(&buff).expect("Writing to socket failed");
+                println!("Message sent: {:?}", msg);
+            },
+            Err(TryRecvError::Empty) => (),
+            Err(TryRecvError::Disconnected) => break
+        }
+        lala();
+    });
+    println!("Write a message:");
+    loop {
+        let mut buff = String::new();
+        io::stdin().read_line(&mut buff).expect("Reading Unable to read message");
+        let msg = buff.trim().to_string();
+        if msg == ":done" || tx.send(msg).is_err() {break}
+    }
+    print!("See you soon!");
+
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+This is a simple take on a chat application built in pure rust.This was an assignment we had to do for school.
+
+This is a basic and simple app,not anything special but it does have a cool name.
+
+The code has two parts:
+
+Server code - This will open a socket and listen to the client messages,This should also store the messages in a Vec.
+Client code - This should run and allow the client to send messages limited to 1000 bytes to the server.
+Future of the project:
+
+Allow another client to join the server and send messages back and forth between the server and other clients.
+A UI built in maybe Slint or any framework that I may deem fit.
+Host the code on AWS or something,preferrably on an EC2 instance.
